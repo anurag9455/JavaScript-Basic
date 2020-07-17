d902822ab5af0751110506db607fb059b3535e95
anurag9455 <anuragsrivastava56789@gmail.com>
2020-07-17 21:32:26 +0530

    Rules addition

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 
-/*alert("GAME RULES
+var rules = "GAME RULES\n- The game has 2 players, playing in rounds\n- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score\n- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn\n- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn\n- The first player to reach 100 points on GLOBAL score wins the game";
+alert(rules);
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game"
-)
-*/
 
 var scores, roundScore, activePlayer, gamePlaying; 
 init()
